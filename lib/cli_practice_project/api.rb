caa1b31ba845f378b10159d69f645d3c4f5be6c2
cep104 <candicelizabethpeters@gmail.com>
2021-03-26 11:31:09 -0500

    json working

--- lib/cli_practice_project/api.rb
+++ lib/cli_practice_project/api.rb
@@ -1,3 +1,7 @@
 class API 
-    
+    def self.get_data 
+        response = RestClient.get('http://api.tvmaze.com/search/shows?q=friends')
+        shows = JSON.parse(response)
+        binding.pry
+    end
 end
